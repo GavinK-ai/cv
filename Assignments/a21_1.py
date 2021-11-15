c4a9fdb4dad4422f2ddb92f85bb12373afbad0d9
JKai <jiannkai_gavin96@hotmail.com>
2021-11-15 20:04:25 +0800

    update

--- Assignments/a21_1.py
+++ Assignments/a21_1.py
@@ -2,8 +2,8 @@
 import numpy as np
 
 #define equation coefficient
-A=[[2,1,1],[2,1,0],[0,2,-3]]
-B=[[1],[1],[1]]
+A=[[2,1,1],[1,1,0],[0,1,-3]]
+B=[[2],[2],[1]]
 
 #solve variables
 x = np.linalg.solve(A,B)
