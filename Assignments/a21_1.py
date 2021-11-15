3e491229be041b3dd3301e7c8d9f24bcb085215c
JKai <jiannkai_gavin96@hotmail.com>
2021-11-15 16:40:36 +0800

    Solving Linear Algebra

--- Assignments/a21_1.py
+++ Assignments/a21_1.py
@@ -1,6 +1,15 @@
+
 import numpy as np
 
+#define equation coefficient
 A=[[2,1,1],[2,1,0],[0,2,-3]]
 B=[[1],[1],[1]]
 
-X = np.linalg.solve(A,B)
+#solve variables
+x = np.linalg.solve(A,B)
+
+
+#for i in range(len(x)):
+#    print(f"x{i+1}:{x[i+1]}\n")
+
+print(x)
