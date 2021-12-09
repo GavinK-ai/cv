c493accb3f43c11a06bb09dcde82a202df45d964
JKai <jiannkai_gavin96@hotmail.com>
2021-12-09 09:13:03 +0800

    Update model_resnet.py

--- myproject/cvPrj/model_resnet.py
+++ myproject/cvPrj/model_resnet.py
@@ -11,7 +11,7 @@ import tensorflow as tf
 from keras.models import Model
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
-from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications import ResNet50,ResNet50V2
 from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -57,19 +57,26 @@ test_image = test_generator.flow_from_dataframe(dataframe=df_test,
                                                 random_seed=RANDOM_SEED
                                                 )
 
-pretrained_model = ResNet50(
+# pretrained_model = ResNet50(
+#     input_shape=(224, 224, 3),
+#     include_top=False,
+#     weights='imagenet',
+#     pooling='max'
+# )
+pretrained_model = ResNet50V2(
     input_shape=(224, 224, 3),
     include_top=False,
     weights='imagenet',
-    pooling='max'
+    pooling='max',
+    classifier_activation="softmax"
 )
 pretrained_model.trainable = False  # We don't want to train again the resnet
 
 inputs = pretrained_model.input
 
 x = Dense(120, activation='relu')(pretrained_model.output)
-x = Dense(120, activation='relu')(x)  # adding some custom layers of our coice
-x = Dense(120, activation='relu')(x)
+#x = Dense(120, activation='relu')(x)  # adding some custom layers of our coice
+#x = Dense(120, activation='relu')(x)
 
 outputs = Dense(2, activation='sigmoid')(x)
 # output choice
@@ -90,19 +97,19 @@ train_acc = mo_fit.history
 pd.DataFrame(mo_fit.history)[['accuracy']].plot()
 plt.title("Train Accuracy")
 plt.show()
-plt.imsave('Training Accuracy.png')
+#plt.imsave('Training Accuracy.png')
 
 pd.DataFrame(mo_fit.history)[['loss']].plot()
 plt.title("Train Loss")
 plt.show()
-plt.imsave('Training Loss.png')
+#plt.imsave('Training Loss.png')
 
 
 print('Test Accuracy')
 eval_result = model.evaluate(test_image)
 
 
-savedFile = f'resnet_aug/cv_image_resnet_{(eval_result[1]*100):.0f}.pt'
+savedFile = f'resnetV2_aug/cv_image_resnetV2_{(eval_result[1]*100):.0f}.pt'
 model.save(savedFile)
 print("Export Path = "+savedFile)
 
